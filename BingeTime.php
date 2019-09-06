e69c3beb76dc5f3563657c3c7435992a9ebb2661
Joseph Shea-Bianco <j.sheabianco@gmail.com>
2019-09-05 18:16:01 -0700

    Documentated the code

--- BingeTime.php
+++ BingeTime.php
@@ -1,69 +1,94 @@
 <?php
 
-$showname = "Fullmetal%20Alchemist%20Brotherhood";
-
+# Includes the api key from a separate file
 include 'apikey.php';
 
+# The name of the show you're looking up
+$showname = "Fullmetal%20Alchemist%20Brotherhood";
+
+# Defines a function to retrieve a JWT token
 function getToken() {
-	global $username, $userkey, $apikey;
+	# Retrieves the apikey from global space
+	global $apikey;
+	# Sets the API endpoint
 	$url = 'https://api.thetvdb.com/login';
 	$data = json_encode(array(
 			'apikey' => $apikey,
-			'userkey' => $userkey,
-			'username' => $username
 	));
 
+	# Build and send the POST request
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
         $response = curl_exec($ch);
+
+	# Close the curl handler and return the JWT token
         curl_close($ch);
 	return json_decode($response)->{'token'};
 }
 
-function makeRequest($token, $url, $data) {
-	$ch = curl_init($url.$data);
+# Defines a function to make a generic API GET request
+function makeRequest($token, $url) {
+	# Build and send the curl handler
+	$ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Bearer '.$token));
         $response = curl_exec($ch);
+
+	# Close the curl handler and return the JSON response
         curl_close($ch);
         return $response;
 }
 
-
-function showLookup($token, $showName) {
-	$url = 'https://api.thetvdb.com/search/series';
-	$data =	"?name=$showName";
-	$response = makeRequest($token, $url, $data);
+# Defines a function to retrieve a show id
+function showLookup($token, $showname) {
+	# Sets the API endpoint
+	$url = "https://api.thetvdb.com/search/series?name=$showname";
+	# Retrieve the JSON response
+	$response = makeRequest($token, $url);
+	# Return the id
 	return json_decode($response)->{'data'}[0]->{'id'};
 }
 
+# Defines a function to count the episodes of a show
 function getEpisodeCount($token, $id) {
+	# Sets the API endpoint
 	$url = "https://api.thetvdb.com/series/$id/episodes";
-	$response = makeRequest($token, $url, '');
+	# Retrieve the JSON response
+	$response = makeRequest($token, $url);
 	$count = 0;
-	$runtime = 0;
+	# Iterate over episodes counting every episodes with an "aired season" greater than 0
 	foreach (json_decode($response)->{'data'} as $episode) {
 		if ($episode->{'airedSeason'} > 0) {
 			$count++;
 		}
 	}
+	
+	# Return the sum of episodes
 	return $count;
 }
 
+# Defines a function to retrieve the listed runtime of a show
 function getRuntime($token, $id) {
+	# Sets the API endpoint
 	$url = "https://api.thetvdb.com/series/$id";
-	$response = makeRequest($token, $url, '');
+	# Retrieve the JSON response
+	$response = makeRequest($token, $url);
+	# Return the listed runtime
 	return json_decode($response)->{'data'}->{'runtime'};
 }
 
+# Get a JWT token
 $token = getToken();
+# Get the show id
 $id = showLookup($token, "$showname");
+# Get the episode count
 $count = getEpisodeCount($token, $id);
+# Get the listed runtime
 $runtime = getRuntime($token, $id);
+# Calculate total runtime and print it out in terms of hours and minutes
 $totalruntime = $runtime*$count;
 print(floor($totalruntime/60)." hours ".($totalruntime%60)." minutes.");
 
